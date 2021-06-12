529c963783327459c92477c07e57bff4d56d0a2f
Sree Gayathri Siddamsetti <sreegayathrisiddamsetti19@gmail.com>
2021-06-12 20:48:57 +0530

    Added all the files for comparision Sorting Algos

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+
+
+var inp_as = document.getElementById('a_size');
+var array_size = inp_as.value;
+var inp_gen = document.getElementById('a_generate');
+var inp_aspeed = document.getElementById('a_speed');
+
+var butts_algos = document.querySelectorAll('#AlgorithmSpecificControls button');
+
+
+var div_sizes = [];
+var divs = [];
+var ps = [];
+var margin_size;
+var cont = document.getElementById('array_container');
+cont.style = "flex-direction: row";
+
+//Array Generation and Updation
+
+inp_gen.addEventListener("click", generate_array);
+inp_as.addEventListener("input", update_array_size);
+
+
+function generate_array() {
+
+  cont.innerHTML = "";
+  for(var i=0; i<array_size; i++) {
+    //var result = floor(Math.random() * 0.5 * (inp_as.max - inp_as.min)) + 10;
+    div_sizes[i] = Math.floor(Math.random() * 0.5 * (inp_as.max - inp_as.min)) + 10;
+    divs[i] = document.createElement("div");
+        
+    cont.appendChild(divs[i]);
+    margin_size = 0.1;
+    divs[i].style = "margin:0%" + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+  }
+}
+
+function update_array_size() {
+  array_size = inp_as.value;
+  generate_array();
+}
+
+window.onload = update_array_size();
+
+for(var i=0; i<butts_algos.length; i++) {
+  butts_algos[i].addEventListener("click", runalgo);
+}
+
+function runalgo() {
+
+  console.log(this.innerHTML);
+
+  this.classList.add("butt_selected");
+  switch(this.innerHTML) {
+    case "Insertion": Insertion_Sort();
+                      break;
+    case "Selection": Selection_Sort();
+                      break;
+    case "Bubble":Bubble_Sort();
+                  break;
+    case "Quick":Quick();
+                  break;
+    case "Merge":Merge();
+                  break;
+    case "Heap":Heap();
+                  break;
+  }
+}
